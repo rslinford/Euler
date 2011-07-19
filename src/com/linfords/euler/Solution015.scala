9ddb1158423ab2c833de463b63cf5f5d283d9079
Scott Linford <scott@linfords.com>
2011-07-19 19:54:22 -0400

    Brute force 015 take 30 hours. Skeleton for 017.

--- src/com/linfords/euler/Solution015.scala
+++ src/com/linfords/euler/Solution015.scala
@@ -1,6 +1,6 @@
 package com.linfords.euler
 
-import com.linfords.euler.Solution011.Point
+import java.lang.Long
 
 /*
 Euler Project Problem 15
@@ -8,27 +8,38 @@ Euler Project Problem 15
 
 Starting in the top left corner of a 2×2 grid, there are 6 routes (without backtracking) to the bottom right corner.
 
-How many routeCount are there through a 20×20 grid?
+How many routes are there through a 20×20 grid?
 
 */
 
-object Solution015 extends App {
+/* Brute force single threaded. Estimated time to completion: ~30 hours */
+object Solution015_v1 extends App {
 
   case class Point(val x: Int, val y: Int)
 
   val startPoint = Point(0, 0)
-  val endPoint = Point(20, 20)
+  val gridSize = 20
+  val endPoint = Point(gridSize, gridSize)
+  var c: Long = 0
 
-  def travel(p: Point, currentRoute: List[Point] = Nil): Int = {
-    if (p == endPoint) 1
-    else if /* out of bounds */ (p.x < startPoint.x || p.y < startPoint.y || p.x > endPoint.x || p.y > endPoint.y) 0
-    else if /* been there */ (currentRoute.contains(p)) 0
-    else if /* back track */ (currentRoute != Nil && (p.x < currentRoute.head.x || p.y < currentRoute.head.y)) 0
-    else (for {v <- List(Point(1, 0), Point(0, 1), Point(-1, 0), Point(0, -1))} yield travel(Point(v.x + p.x, v.y + p.y), p :: currentRoute)).sum
+  def travel(p: Point, route: List[Point] = Nil): Long = {
+    c += 1
+    val r = p :: route
+
+    if (p == endPoint) {
+      if (c % 1000000000 < 20) println(c + "] " + r.reverse);
+      1
+    }
+    else if (p.x > endPoint.x || p.y > endPoint.y) 0
+    else (for {v <- List(Point(1, 0), Point(0, 1))} yield travel(Point(v.x + p.x, v.y + p.y), p :: route)).foldLeft(new Long(0))(_ + _)
   }
 
-  def solve: Int = {
-    travel(startPoint)
+  def solve: Long = {
+    val startTime = System.currentTimeMillis();
+    val result = travel(startPoint)
+    val elapsedTime = System.currentTimeMillis() - startTime;
+    println("Elapsed minutes: " + elapsedTime / 1000 / 60.0)
+    result
   }
 
   println("Answer: " + solve)
