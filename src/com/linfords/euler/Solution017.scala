efb2113a05c9421580d0945fa12be8c52f089c6b
Scott Linford <scott@linfords.com>
2011-07-21 15:42:47 -0400

    017 complete

--- src/com/linfords/euler/Solution017.scala
+++ src/com/linfords/euler/Solution017.scala
@@ -16,7 +16,7 @@ contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The u
 */
 
 object Solution017 extends App {
-  def writeNumber(number: Int) = number match {
+  def smallNumber(number: Int) = number match {
     case 1 => "one"
     case 2 => "two"
     case 3 => "three"
@@ -35,17 +35,47 @@ object Solution017 extends App {
     case 16 => "sixteen"
     case 17 => "seventeen"
     case 18 => "eighteen"
-    case 19 => "ninteen"
+    case 19 => "nineteen"
   }
 
-  def wn(number: Int) = {
+  def tens(number: Int) = number match {
+    case 2 => "twenty"
+    case 3 => "thirty"
+    case 4 => "forty"
+    case 5 => "fifty"
+    case 6 => "sixty"
+    case 7 => "seventy"
+    case 8 => "eighty"
+    case 9 => "ninety"
+  }
 
+  def separate(s: String) = if (s.length() > 0) " " else ""
+
+  def separateAnd(number: Int) = if (number == 0) "" else " and"
+
+  def countAlpha(s: String) = "[a-z]".r.findAllIn(s).map(_.length()).sum
+
+  def numberToEnglish(number: Int, builder: String = ""): String = {
+    if (number > 999) numberToEnglish(number % 1000, builder + separate(builder) + smallNumber(number / 1000) + " thousand")
+    else if (number > 99) numberToEnglish(number % 100, builder + separate(builder) + smallNumber(number / 100) + " hundred" + separateAnd(number % 100))
+    else if (number > 19) numberToEnglish(number % 10, builder + separate(builder) + tens(number / 10))
+    else if (number > 0) numberToEnglish(0, builder + separate(builder) + smallNumber(number))
+    else builder
   }
 
   def solve: Int = {
-    (1 to 1000).map(writeNumber(_)).foreach(println(_))
-    1234
+    (1 to 1000).map(numberToEnglish(_)).map(countAlpha(_)).sum
   }
 
   println("Answer: " + solve)
+}
+
+object Unit0117 extends App {
+  assert(Solution017.numberToEnglish(115) == "one hundred and fifteen");
+  assert(Solution017.countAlpha(Solution017.numberToEnglish(115)) == 20);
+  val s342 = Solution017.numberToEnglish(342)
+  assert(s342 == "three hundred and forty two");
+  assert(Solution017.countAlpha(s342) == 23);
+
+  println("All systems go")
 }
