506ab7b8778025a067adcccc71248c453eee6406
Scott Linford <scott@linfords.com>
2011-07-23 12:56:54 -0400

    023 complete, solution is memory hungry and took 117 seconds

--- src/com/linfords/euler/Solution023.scala
+++ src/com/linfords/euler/Solution023.scala
@@ -1,5 +1,7 @@
 package com.linfords.euler
 
+import annotation.tailrec
+
 /*
 Euler Project Problem 23
 02 August 2002
@@ -32,12 +34,53 @@ object Solution023 extends App with Code023 {
 }
 
 trait Code023 {
-  def solve = 1234
+  val provenCutoff = 28123
+  val abundantList = abundantNumbers().takeWhile(_ < provenCutoff).toList
+
+  def properDivisors(n: Int) = n match {
+    case 0 => List()
+    case 1 => List(1)
+    case _ => (1 to n / 2).filter(n % _ == 0).toList
+  }
+
+  def abundantNumbers(n: Int = 1): Stream[Int] = {
+    if (properDivisors(n).sum > n) Stream.cons(n, abundantNumbers(n + 1))
+    else abundantNumbers(n + 1)
+  }
+
+  def crossAdd(a: List[Int]): List[Int] = {
+    @tailrec
+    def recurse(a1: List[Int], b1: List[Int], acum: List[Int]): List[Int] = {
+      val (nextA, nextB) = if (a1.tail == Nil) (a, b1.tail) else (a1.tail, b1)
+      if (nextB == Nil) a1.head + b1.head :: acum
+      else recurse(nextA, nextB, a1.head + b1.head :: acum)
+    }
+    recurse(a, a, Nil)
+  }
+
+  def solve = {
+    val abundantSums = crossAdd(abundantList).toSet
+    (1 until provenCutoff).filter(!abundantSums.contains(_)).sum
+  }
 }
 
 object Unit023 extends Code023 {
   def run = {
-    assert(1 == 1)
+    assert(abundantNumbers()(0) == 12)
+    assert(abundantList.last == provenCutoff - 1)
+
+    val ls = List(2, 7, 34)
+    val ca = crossAdd(ls)
+    assert(ca(0) == 68)
+    assert(ca(1) == 41)
+    assert(ca(2) == 36)
+    assert(ca(3) == 41)
+    assert(ca(4) == 14)
+    assert(ca(5) == 9)
+    assert(ca(6) == 36)
+    assert(ca(7) == 9)
+    assert(ca(8) == 4)
+
     println("All systems go!")
   }
 }
